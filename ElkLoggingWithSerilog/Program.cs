546fb15f82cef7b83535e6cfd39a86d6224e0583
paul <paul_silvester10@yahoo.com>
2021-08-19 20:28:54 +0100

    main basic logging

---
+++ ElkLoggingWithSerilog/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+namespace ElkLoggingWithSerilog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // The initial "bootstrap" logger is able to log errors during start-up. It's completely replaced by the
+            // logger configured in `UseSerilog()` below, once configuration and dependency-injection have both been
+            // set up successfully.
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
+            Log.Information("Starting up!");
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseSerilog((hostingContext, loggerConfiguration) =>
+                loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
+    }
+}
